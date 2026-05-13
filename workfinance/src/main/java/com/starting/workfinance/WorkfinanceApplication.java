231e4a931b111a097e63a8bd4e396403427eb29d
devjoaopr <joaoprsantos1@outlook.com>
2026-05-12 21:33:14 -0300

    starting the project

---
+++ workfinance/src/main/java/com/starting/workfinance/WorkfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.starting.workfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkfinanceApplication.class, args);
+	}
+
+}
---
+++ workfinance/src/test/java/com/starting/workfinance/WorkfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starting.workfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
